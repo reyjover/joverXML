aa808d8123febb906daf7e3cf3a2991d353bd889
reyjover <60122552+reyjover@users.noreply.github.com>
2020-01-28 11:07:44 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xml-act1.herokuapp.com/");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("horses);
+ 
+ ?>
+ <ul>
+    <?php
+ foreach( $content as $data )
+ {
+   $title = $data->getElementsByTagName("name")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("coat")->item(0)->nodeValue;
+  
+   echo "<li>$name
+            <ul>
+                <li>$coat</li>
+            </ul>
+        </li>";
+ }
+?>
+</ul>
